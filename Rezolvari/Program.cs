5fe77e3a72c0187ce8da76dc6c02cb9e4e3526c2
Andrei L <lazar.andrei@outlook.com>
2019-11-16 13:03:25 +0200

    mesaj

---
+++ Rezolvari/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rezolvari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
