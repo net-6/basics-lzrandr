9f0cfd07a40151e2fb89ef88621f60b0fbfd4c6b
Andrei L <lazar.andrei@outlook.com>
2019-11-16 13:37:04 +0200

    initial commit

--- Rezolvari/Program.cs
+++ Rezolvari/Program.cs
@@ -6,9 +6,29 @@ namespace Rezolvari
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
-            
+            Sum();
+        
         }
+
+        //1. Write a method that reads from the console three numbers of type int and prints their sum.
+        static void Sum()
+        {
+            Console.WriteLine("Introduceti primul numar:");
+            String no1 = Console.ReadLine();
+            Console.WriteLine("Introduceti al doilea numar:");
+            String no2 = Console.ReadLine();
+            Console.WriteLine("Introduceti al treilea numar:");
+            String no3 = Console.ReadLine();
+            int nr1 = int.Parse(no1);
+            int nr2 = int.Parse(no2);
+            int nr3 = int.Parse(no3);
+            Console.WriteLine("Suma numerelor introduse este: " + (nr1 + nr2 + nr3));
+        }
+
+
+
+
+
+
     }
 }
